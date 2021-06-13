5bb42c6d794cc792272856df211381cd407e8be5
Nikita Kazakov <djnikitos98@gmail.com>
2021-06-13 13:33:43 +0400

    Добавьте файлы проекта.

---
+++ Model/Model.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace SimbirSoftTestTask.Model
+{
+    public static class Model
+    {
+
+        private static readonly char[] Separators = {
+            ' ', ',', '.', '!', '?','"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t', '-'
+        };
+        
+        public static Dictionary<string, int> GetStatistic(string url)
+        {
+            string htmlString;
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+            
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream ?? throw new InvalidOperationException()))
+                {
+                    htmlString = reader.ReadToEnd();
+                }
+            }
+
+            response.Close();
+
+            string[] words = GetPlainTextFromHtml(htmlString).Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> statistic = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (statistic.TryGetValue(word, out int count))
+                {
+                    _ = statistic.Remove(word);
+                    statistic.Add(word, ++count);
+                }
+                else
+                {
+                    statistic.Add(word, 1);
+                }
+            }
+
+            return statistic;
+        }
+
+        private static string GetPlainTextFromHtml(string htmlString)
+        {
+            Regex regexJsCss = new Regex("(\\<script(.+?)\\</script\\>)|(\\<style(.+?)\\</style\\>)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            Regex regexHtmlTags = new Regex("(<.*?>)+", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+            htmlString = regexJsCss.Replace(htmlString, string.Empty);
+            htmlString = regexHtmlTags.Replace(htmlString, string.Empty);
+            htmlString = Regex.Replace(htmlString, "\\s\\s+", " ");
+            htmlString = htmlString.Replace("&nbsp;", string.Empty);
+
+            return htmlString;
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using SimbirSoftTestTask.Util;
+
+namespace SimbirSoftTestTask.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+        private string _url;
+
+        private Dictionary<string, int> _statistic;
+
+        private bool _isUrlValid;
+
+        private RelayCommand _getStatisticRelayCommand;
+
+        public string Url
+        {
+            get => _url;
+            set
+            {
+                IsUrlValid = Uri.TryCreate(value, UriKind.Absolute, out Uri uriResult)
+                             && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+                _url = value;
+                OnPropertyChanged(nameof(Url));
+            }
+        }
+
+        public Dictionary<string, int> Statistic
+        {
+            get => _statistic;
+
+            set
+            {
+                _statistic = value;
+                OnPropertyChanged(nameof(Statistic));
+            }
+        }
+
+        public bool IsUrlValid
+        {
+            get => _isUrlValid;
+            set
+            {
+                _isUrlValid = value;
+                OnPropertyChanged(nameof(IsUrlValid));
+            }
+        }
+        
+        public RelayCommand GetStatisticRelayCommand =>
+            _getStatisticRelayCommand ?? (_getStatisticRelayCommand = new RelayCommand(obj =>
+            {
+                Statistic = Model.Model.GetStatistic(Url);
+            }));
+    }
+}
